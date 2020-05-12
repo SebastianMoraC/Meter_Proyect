4d4fadea7d8b10967c343283d249a69af10fd6f9
Sebastian <Sebastian@192.168.39.190>
2020-05-12 14:52:57 -0500

    Update README.md43t

--- src/main/java/co/utp/meter_proyect/Ejemplo/Ejemplo.java
+++ src/main/java/co/utp/meter_proyect/Ejemplo/Ejemplo.java
@@ -11,7 +11,6 @@
 public class Ejemplo {
     public static void main(String[] args) {
         System.out.println("eed");
-        System.out.println("My love");
-        int x;
+        
     }
 }
