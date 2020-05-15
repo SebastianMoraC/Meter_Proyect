430e5d30fadbb5157250ddc830f3667d34165920
Sebastian <Sebastian@192.168.39.190>
2020-05-15 08:03:29 -0500

    Add MVC

---
+++ src/main/java/co/utp/meter/Main.java
@@ -0,0 +1,18 @@
+package co.utp.meter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("eed");
+        
+    }
+}
--- src/main/java/co/utp/meter/Model/Meter_Model.java
+++ src/main/java/co/utp/meter/Model/Meter_Model.java
@@ -1,18 +1,14 @@
-package co.utp.meter;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package co.utp.meter.Model;
 
 /**
  *
  * @author Sebastian
  */
-public class Ejemplo {
-    public static void main(String[] args) {
-        System.out.println("eed");
-        
-    }
+public class Meter_Model {
+    
 }
