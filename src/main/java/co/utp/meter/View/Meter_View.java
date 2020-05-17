a156f330710aa26e36fab3b3ce582862d0d6c13e
Sebastian <Sebastian@192.168.39.190>
2020-05-17 11:21:34 -0500

    Window version 1.1

--- src/main/java/co/utp/meter/View/Meter_View.java
+++ src/main/java/co/utp/meter/View/Meter_View.java
@@ -2,6 +2,10 @@ package co.utp.meter.View;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Image;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -39,7 +43,6 @@ public class Meter_View extends JFrame{
         this.setLocationRelativeTo(null); //Centra la ventana en la pantalla
         this.setTitle("WaE Meter Consumption");
         this.setMinimumSize(new Dimension(700,600)); //Set a minimum size to the frame
-
         startComponents();
     }
     public void render(){
@@ -49,12 +52,40 @@ public class Meter_View extends JFrame{
     
      public void startComponents(){
         panels();
+        buttons();
     }
      
     public void panels(){
         panel = new JPanel();
-        panel.setBackground(Color.gray); //Set the color in the panel
+        panel.setLayout(null);
+        panel.setBackground(Color.LIGHT_GRAY); //Set the color in the panel
         this.getContentPane().add(panel); //Insert the panel to the frame
 
+    }
+    public void buttons(){
+        JButton energyButton = new JButton();
+        energyButton.setEnabled(true);
+        energyButton.setBounds(100,300,200,200);
+        energyButton.setBorder(BorderFactory.createEmptyBorder()); //Border of image
+        ImageIcon buttonEnergy = new ImageIcon("electricidad.png"); //Get Image
+        energyButton.setIcon(new ImageIcon(buttonEnergy.getImage().getScaledInstance(energyButton.getWidth(), energyButton.getHeight(),Image.SCALE_SMOOTH))); //Insert image in the button with the size of the button 
+        energyButton.setBackground(Color.lightGray);
+        panel.add(energyButton);
+        
+        JButton waterButton = new JButton();
+        waterButton.setEnabled(true);
+        waterButton.setBounds(400,300,200,200);
+        waterButton.setBorder(BorderFactory.createEmptyBorder()); //Border of image
+        ImageIcon buttonImage = new ImageIcon("agua.png"); //Get Image
+        waterButton.setIcon(new ImageIcon(buttonImage.getImage().getScaledInstance(waterButton.getWidth(), waterButton.getHeight(),Image.SCALE_SMOOTH))); //Insert image in the button with the size of the button 
+        waterButton.setBackground(Color.lightGray);
+        panel.add(waterButton);
+        
+        
+        
+    }
+
+    private void setMaximumSize(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
